a3c2ccd3c874251bb88f1e7305b29fb865514f5e
Manshi Todi <manshitodi@gmail.com>
2021-01-25 18:56:30 +0530

    added database

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin_list = [CustomUser, UserDetail, ]
+
+for site in admin_list:
+    admin.site.register(site)
--- hostel_management/settings.py
+++ hostel_management/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'home.CustomUser'
 
 # Application definition
 
