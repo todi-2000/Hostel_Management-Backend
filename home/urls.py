f73f4a2c5f3fe936875938d31b8c6fa8e540db91
Manshi Todi <manshitodi@gmail.com>
2021-01-29 10:31:15 +0530

    signup

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('signup/', signup, name ='signup')
 ]
 
